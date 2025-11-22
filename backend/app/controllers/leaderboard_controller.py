ee4e0d9e75d1ffc673234bfe3fe669ce130d2079
Zubair Idris Aweda <zubairidrisaweda@gmail.com>
2025-11-22 13:50:02 +0000

    feat: leaderboard

---
+++ backend/app/controllers/leaderboard_controller.py
@@ -0,0 +1,22 @@
+from flask import jsonify
+from app.controllers.auth_controller import token_required
+from app.services.leaderboard_service import LeaderboardService
+
+class LeaderboardController:
+    @staticmethod
+    def get_leaderboard():
+        leaderboard = LeaderboardService.get_leaderboard()
+
+        return jsonify({
+            "message": "Leaderboard retrieved successfully",
+            "data": [entry.serialize() for entry in leaderboard]
+        }), 200
+
+    @staticmethod
+    def get_wlv_leaderboard():
+        wlv_leaderboard = LeaderboardService.get_wlv_leaderboard()
+
+        return jsonify({
+            "message": "WLv Leaderboard retrieved successfully",
+            "data": [entry.serialize() for entry in wlv_leaderboard]
+        }), 200
